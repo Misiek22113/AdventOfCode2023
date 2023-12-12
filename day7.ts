eb57e3fce5758a1ef210cbf4584be7c5ad925b2a
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-12-12 21:28:43 +0100

    finish day 7 part one

---
+++ day7.ts
@@ -0,0 +1,113 @@
+import fs from "fs";
+
+type hand = {
+  hand: string;
+  bid: number;
+  result: number;
+  rank: number;
+};
+
+const inputData = fs.readFileSync("data_day7.txt", "utf-8");
+
+const games = inputData.split("\n");
+
+let hands: hand[] = [];
+
+const findLayout = ({ cards }: { cards: number[] }): number => {
+  const max = Math.max(...cards);
+  let filteredPairs = cards.filter((card) => card == 2);
+  let rank = 0;
+  if (max == 5) {
+    rank = 6;
+  } else if (max == 4) {
+    rank = 5;
+  } else if (cards.includes(3) && cards.includes(2)) {
+    rank = 4;
+  } else if (max == 3) {
+    rank = 3;
+  } else if (filteredPairs.length == 2) {
+    rank = 2;
+  } else if (filteredPairs.length == 1) {
+    rank = 1;
+  }
+  return rank;
+};
+
+games.map((game) => {
+  const data = game.trim().split(" ");
+  const cards = data[0].split("");
+  let cardsCounter: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  cards.map((card) => {
+    if (card >= "2" && card <= "9") {
+      let index = parseInt(card);
+      cardsCounter[index] += 1;
+    } else {
+      if (card === "A") {
+        cardsCounter[14] += 1;
+      } else if (card === "K") {
+        cardsCounter[13] += 1;
+      } else if (card === "Q") {
+        cardsCounter[12] += 1;
+      } else if (card === "J") {
+        cardsCounter[11] += 1;
+      } else if (card === "T") {
+        cardsCounter[10] += 1;
+      }
+    }
+  });
+  hands.push({
+    hand: data[0],
+    bid: parseInt(data[1]),
+    result: 0,
+    rank: findLayout({ cards: cardsCounter }),
+  });
+});
+
+let result = 0;
+
+const res = hands.sort((card1, card2) => {
+  const vars = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "T",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  if (card1.rank === card2.rank) {
+    let charsCard1 = card1.hand.split("");
+    let charsCard2 = card2.hand.split("");
+    for (let i = 0; i < charsCard1.length; i++) {
+      if (charsCard1.at(i) === charsCard2.at(i)) {
+        continue;
+      } else if (
+        vars.indexOf(charsCard1.at(i) as string) >
+        vars.indexOf(charsCard2.at(i) as string)
+      ) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  } else if (card1.rank < card2.rank) {
+    return -1;
+  } else {
+    return 1;
+  }
+  return 0;
+});
+
+res.map((hand, index) => {
+  console.log(hand.bid, " * ", index + 1);
+  result += hand.bid * (index + 1);
+});
+
+console.log(res, result);
